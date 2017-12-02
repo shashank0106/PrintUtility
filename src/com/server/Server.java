6cfc3daf6c7f0171e452fcaf7cc9c572c42251f8
shashank0106 <shashankgupta104@gmail.com>
2017-12-02 21:35:20 +0530

    changing to multithreaded

--- src/com/server/Server.java
+++ src/com/server/Server.java
@@ -6,7 +6,7 @@ import java.net.Socket;
 
 public class Server {
 
-    static final int PORT = 1978;
+    static final int PORT = 7655;
 
     public static void main(String args[]) {
         ServerSocket serverSocket = null;
